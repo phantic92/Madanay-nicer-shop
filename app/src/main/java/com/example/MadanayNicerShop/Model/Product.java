ce3e32c8b5d91837715c0468cd5bec3a44a0a7a4
Darrell-David Madanay <darrellmadanay@gmail.com>
2019-10-11 10:34:25 -0400

    Created attributes for model

--- app/src/main/java/com/example/MadanayNicerShop/Model/Product.java
+++ app/src/main/java/com/example/MadanayNicerShop/Model/Product.java
@@ -1,4 +1,49 @@
 package com.example.MadanayNicerShop.Model;
 
 public class Product {
+    private String title;
+    private String description;
+    private String price;
+    private String quantity;
+    private int image;
+
+    public Product(String title, String description, String price, String quantity, int image) {
+        this.title = title;
+        this.description = description;
+        this.price = price;
+        this.quantity = quantity;
+        this.image = image;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public String getPrice() {
+        return price;
+    }
+    public String getQuantity() {
+        return quantity;
+    }
+    public int getImage() {
+        return image;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public void setPrice(String price) {
+        this.price = price;
+    }
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+    public void setImage(int image) {
+        this.image = image;
+    }
 }
