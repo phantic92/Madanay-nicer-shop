337b3c59b5cf5316a69783097f23309fc6f5860d
Darrell-David Madanay <darrellmadanay@gmail.com>
2019-10-11 10:14:18 -0400

    Added model package

---
+++ app/src/main/java/com/example/MadanayNicerShop/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.MadanayNicerShop;
+
+/**
+ * Author: Darrell-David Madanay
+ * since 09/20/2019
+ * Main Activity for shopping app
+ */
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Starts menu activity
+     * @param view View
+     */
+    public void startMenuActivity(View view) {
+        Intent intent = new Intent(this, MenuActivity.class);
+        Log.d(TAG, "Clicked on \"Start>\" button");
+        startActivity(intent);
+    }
+}
