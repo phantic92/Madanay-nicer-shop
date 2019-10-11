d0140416adbcbac5d614f87911b8775292b18b14
Darrell-David Madanay <darrellmadanay@gmail.com>
2019-10-11 11:41:40 -0400

    Created product objects in menu activity

--- app/src/main/java/com/example/MadanayNicerShop/MenuActivity.java
+++ app/src/main/java/com/example/MadanayNicerShop/MenuActivity.java
@@ -3,13 +3,20 @@ package com.example.MadanayNicerShop;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.MadanayNicerShop.Model.Product;
+
+import java.util.LinkedList;
+
 public class MenuActivity extends AppCompatActivity {
+    private LinkedList<Product> mProductList = new LinkedList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +25,15 @@ public class MenuActivity extends AppCompatActivity {
 //        Toolbar toolbar = findViewById(R.id.toolbar);
 //        setSupportActionBar(toolbar);
 
+        // Create products
+        mProductList.addLast(new Product(getString(R.string.barbell_title), getString(R.string.barbell_description),
+                getString(R.string.barbell_price_tag), getString(R.string.barbell_quantity),
+                R.drawable.olympic_barbell));
+        mProductList.addLast(new Product(getString(R.string.eleiko_title), getString(R.string.eleiko_description),
+                getString(R.string.eleiko_price_tag), getString(R.string.eleiko_quantity),
+                R.drawable.eleiko_plates));
+        mProductList.addLast(new Product(getString(R.string.squat_stand_title), getString(R.string.squat_rack_description),
+                getString(R.string.squat_rack_price_tag), getString(R.string.squat_rack_quantity), R.drawable.squat_rack));
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
